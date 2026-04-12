26ce9838632dca39f4db0ac07213b6fff2c02f47
caophihung1234 <caophihung1234@gmail.com>
2026-04-12 14:19:20 +0700

    update

--- cogs/jail.py
+++ cogs/jail.py
@@ -2,6 +2,7 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 from utils import jail_user, get_time, convert_time
+import asyncio
 
 class jail(commands.Cog):
     def __init__(self, bot):
@@ -29,14 +30,14 @@ class jail(commands.Cog):
         if not (member.id in self.whitelist or member.top_role >= interaction.guild.me.top_role):
             try:
                 jail_time = convert_time(time)
-                await jail_user(interaction, member, get_time(), jail_time, self.jail_role)
+                asyncio.create_task(jail_user(interaction, member, get_time(), jail_time, self.jail_role))
                 channel = interaction.guild.get_channel(self.chay_to)
                 reason = f"\nVới lý do: ```{reason}```" if reason else ''
                 message = f"`@{member.display_name}` vừa bị tù ngay với thời gian là: `{time}`{reason}\nThời gian ra tù: <t:{int(get_time() + jail_time)}:R>"
                 await interaction.response.send_message(embed=discord.Embed(title=message, color=0xff0000))
                 await channel.send(member.mention, embed=discord.Embed(title=message, color=0xff0000))
             except:
-             await interaction.response.send_message(embed=discord.Embed(title=f"Đã có lỗi xảy ra!", color=0xff0000), ephemeral=True)
+                await interaction.response.send_message(embed=discord.Embed(title=f"Đã có lỗi xảy ra!", color=0xff0000), ephemeral=True)
 
 async def setup(bot):
     await bot.add_cog(jail(bot))
