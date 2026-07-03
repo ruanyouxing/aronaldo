03487c54376a3c3c87744bf4b1be7f415c65d2cc
rice-cracker-dev <ricecracker2234@proton.me>
2026-07-03 23:08:46 +0700

    nuke commission

--- flake.nix
+++ flake.nix
@@ -1,35 +1,18 @@
 {
-  description = "env setup";
+  description = "Description for the project";
 
   inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: 
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-      
-      myPythonEnv = pkgs.python313.withPackages (p: with p; [
-        discordpy
-        gspread
-        google-auth
-        python-dotenv
-      ]);
-
-    in {
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = [
-          myPythonEnv
-          pkgs.pyright   
-          pkgs.black
-        ];
-      };
-      apps.${system}.default = {
-        type = "app";
-        program = "${pkgs.writeShellScriptBin "run-app" ''
-          ${myPythonEnv}/bin/python ./main.py
-        ''}/bin/run-app";
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
+      perSystem = {pkgs, ...}: {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [uv];
+        };
       };
     };
 }
