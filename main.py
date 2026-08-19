9b33e04f0b332d44ee4cad5cb8ab5648467bb30d
ruanyouxing <ruanyouxing7@gmail.com>
2026-08-19 11:07:49 +0700

    fix ssl cert error in deployment

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 '''dev by caophihung'''
 
+import os
+os.environ.setdefault("SSL_CERT_FILE", __import__("certifi").where())
+
 import discord
 from discord.ext import commands
-import os
 import json
 from dotenv import load_dotenv
 from utils import load_cogs
